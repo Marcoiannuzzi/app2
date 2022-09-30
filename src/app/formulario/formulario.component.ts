0ee6bd8a2b28ab92aa0b62c55b0d3e0e743282f0
Marco Iannuzzi <marcoiannuzzi@live.com.ar>
2022-09-30 16:02:05 -0300

    FORMULARIO REACTIVO

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+  formAlumno!:FormGroup;
+
+  constructor( private fb:FormBuilder) {
+    this.formAlumno = this.fb.group({
+      nombre:new FormControl ('', [Validators.required, Validators.minLength(3)]),
+      apellido: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$')]),
+      curso: new FormControl('', [Validators.required ,Validators.minLength(2)]),
+   });
+  }
 
   ngOnInit(): void {
+  }
+
+  registrarAlumno():void{
+  
+    return console.log(this.formAlumno.value);
+
   }
 
 }
